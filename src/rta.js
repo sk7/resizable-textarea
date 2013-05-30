22d5e7ff236aacdce8b01f3bfff1740bad2c607a
Jayce Pulsipher <jaycemp@gmail.com>
2013-05-30 13:21:35 -0600

    Used clone element to track height

--- src/rta.js
+++ src/rta.js
@@ -1,11 +1,54 @@
-var ev = require('event');
+var ev = require('event')
+  , style = require('computed-style')
+  , emptyLineReg = /(\n|\r|\n\r)$/g
+  , styleList = [
+      'font-size'
+    , 'font-style'
+    , 'font-weight'
+    , 'font-family'
+    , 'line-height'
+    , 'text-transform'
+    , 'letter-spacing'
+    , 'width'
+  ]
+;
 
-module.exports = function (element) {
+function duplicateStyles(dup, el) {
+  var i = 0
+    , l = styleList.length
+  ;
+  while (i < l) {
+    dup.style[styleList[i]] = style(el)[styleList[i]];
+    i += 1;
+  }
+}
+
+module.exports = function (element, options) {
+
+  options = options || {};
+
+  var duplicate = document.createElement('div')
+    , min = options.min || 20
+    , max = options.max || Infinity
+  ;
+
+  duplicate.className = 'resizable-textarea';
+
+  duplicateStyles(duplicate, element);
+  document.body.appendChild(duplicate);
+
   ev.bind(element, 'input', function () {
-    rows = parseInt(this.getAttribute('rows'));
-    var scrollHeight = this.scrollHeight;
-    var height = this.clientHeight;
     
-    this.setAttribute('rows', Math.ceil(((scrollHeight * rows) / height)))
+    // If the last line is empty, then add in a space to match textarea
+    duplicate.textContent = element.value.replace(emptyLineReg, '$1&nbsp;');
+    
+    var height = parseInt(style(duplicate).height, 10);
+    
+    element.style.height = Math.max(min, Math.min(max, height)) + 'px';
+
   });
+
+  // set the minimum rows
+  element.style.height = min + 'px';
+
 };
